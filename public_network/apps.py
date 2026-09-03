fecaa7a71962156678cadfa2432e77ccb6fd775f
javaher9 <javaher9@gmail.com>
2026-09-03 12:46:06 +0330

    Add public network app config

---
+++ public_network/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class PublicNetworkConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'public_network'
+    verbose_name = 'شبکه عمومی گرین لایف'
