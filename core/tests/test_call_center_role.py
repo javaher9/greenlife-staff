c558dcc400474e41783672892e63ff9d9c521853
javaher9 <javaher9@gmail.com>
2026-09-06 19:29:28 +0330

    Include appointment regressions in existing CI suite

--- core/tests/test_call_center_role.py
+++ core/tests/test_call_center_role.py
@@ -1,3 +1,4 @@
+from core.tests.test_appointments import SharedAppointmentTests as SharedAppointmentRegressionTests
 from django.contrib.auth.models import User
 from django.test import TestCase
 from django.urls import reverse
