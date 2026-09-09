da61d97c97d4de57a062258a07d9cce19e674737
javaher9 <javaher9@gmail.com>
2026-09-09 16:55:41 +0330

    Test compact million-toman revenue formatting

--- core/tests/test_real_revenue_snapshot.py
+++ core/tests/test_real_revenue_snapshot.py
@@ -12,12 +12,18 @@ class RealClinicRevenueSnapshotTests(SimpleTestCase):
         self.assertTrue(snapshot['available'])
         self.assertEqual(snapshot['record_count'], 43)
         self.assertEqual(snapshot['grand_total'], 3_947_000_000)
+        self.assertEqual(snapshot['grand_total_display'], '3947')
 
         totals = {row['name']: row['total'] for row in snapshot['branches']}
         self.assertEqual(totals['نیاوران'], 2_725_000_000)
         self.assertEqual(totals['پونک'], 794_000_000)
         self.assertEqual(totals['اصفهان'], 426_000_000)
         self.assertEqual(totals['ارومیه'], 2_000_000)
+        displays = {row['name']: row['total_display'] for row in snapshot['branches']}
+        self.assertEqual(displays['نیاوران'], '2725')
+        self.assertEqual(displays['پونک'], '794')
+        self.assertEqual(displays['اصفهان'], '426')
+        self.assertEqual(displays['ارومیه'], '2')
 
     def test_missing_branch_day_is_not_converted_to_zero(self):
         snapshot = _clinic_revenue_snapshot(date(2026, 9, 9))
