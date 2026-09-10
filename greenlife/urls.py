607a10f55271bd3b0d6a64a996dd861700667885
javaher9 <javaher9@gmail.com>
2026-09-10 21:23:55 +0330

    Route Instagram lead landing page

--- greenlife/urls.py
+++ greenlife/urls.py
@@ -2,8 +2,11 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from core import instagram_views
+
 urlpatterns=[
     path('admin/',admin.site.urls),
+    path('instagram/',instagram_views.instagram_lead,name='instagram_lead'),
     path('',include('public_network.urls')),
     path('',include('core.urls')),
 ]
