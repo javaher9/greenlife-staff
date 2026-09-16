e12fd87156b64867af5686ab8667c4cfcc2209aa
javaher9 <javaher9@gmail.com>
2026-09-16 16:19:39 +0330

    Route consultant sales outcome panel

--- greenlife/urls.py
+++ greenlife/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
-from core import call_center_views, instagram_views, lead_ingest_views, lead_management_views
+from core import call_center_views, consultant_sales_views, instagram_views, lead_ingest_views, lead_management_views
 
 urlpatterns=[
     path('admin/',admin.site.urls),
@@ -12,6 +12,7 @@ urlpatterns=[
     path('instagram/manual/',instagram_views.instagram_manual_lead,name='instagram_manual_lead'),
     path('call-center/leads/<int:pk>/call-started/',call_center_views.mark_call_started,name='call_center_mark_call_started'),
     path('lead-management/',lead_management_views.lead_management_dashboard,name='lead_management_dashboard'),
+    path('consultant/sales/',consultant_sales_views.consultant_sales_outcomes,name='consultant_sales_outcomes'),
     path('',include('public_network.urls')),
     path('',include('core.urls')),
 ]
