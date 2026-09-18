8040d1e403e00e369eee13224659d3976b5fd444
javaher9 <javaher9@gmail.com>
2026-09-18 23:29:17 +0330

    Avoid strict HTML parser in source-column regression

--- core/tests/test_instagram_page_sources.py
+++ core/tests/test_instagram_page_sources.py
@@ -92,4 +92,4 @@ class InstagramPageSourceTests(TestCase):
         self.assertContains(hub, 'آمار پیج‌های اینستاگرام')
         self.assertContains(hub, 'Greenlife.cafe')
         self.assertContains(hub, 'Greenlife.cafe')
-        self.assertNotContains(hub, '<th>منبع</th>', html=True)
+        self.assertNotContains(hub, '<th>منبع</th>')
